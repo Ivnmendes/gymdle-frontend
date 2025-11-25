ba96b0074aa2689f910843fb260067ebd85f12e3
Ivan Mendes <ivanmendes2005@gmail.com>
2025-11-25 13:11:46 -0300

    fix: Arrumar env url do servidor

--- gymdle/src/services/api.js
+++ gymdle/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.URL_SERVIDOR + '/api', 
+  baseURL: import.meta.env.VITE_URL_SERVIDOR + '/api', 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
