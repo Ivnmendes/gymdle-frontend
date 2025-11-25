72fa4b65945777bb283811ff7ecf7197c32a4727
Ivan Mendes <ivanmendes2005@gmail.com>
2025-11-25 12:20:37 -0300

    feat: Implementar url dinamica servidor

--- gymdle/src/services/api.js
+++ gymdle/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', 
+  baseURL: import.meta.env.URL_SERVIDOR + '/api', 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
